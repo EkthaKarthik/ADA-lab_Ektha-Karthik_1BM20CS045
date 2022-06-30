972d18d1f221486bd0ea72e23da5b54cba2f0a83
EkthaKarthik <84076593+EkthaKarthik@users.noreply.github.com>
2022-06-30 09:53:11 +0530

    Update insertion_sort.c

--- insertion_sort.c
+++ insertion_sort.c
@@ -1,29 +1,50 @@
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+void insertionSort( long int arr[], float n)
+{
+  int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+  
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+int main()
+{
+    long int n = 10000;
+    int it = 0;
+  
 
-   
-   int i, j, count, temp, number[25];
+    double tim2[10];
+  
+    printf("Array size, Time taken for insertion sort\n");
+  
+    while (it++ < 10) {
+     long int b[n];
+  
+     
+        for (int i = 0; i < n; i++) {
+            long int no = rand() % n + 1;
+            b[i] = no;
+        }
+  
+        clock_t start, end;
+        
+         start = clock();
+        insertionSort(b, n);
+        end = clock();
+  
+        tim2[it] += ((double)(end - start)/CLOCKS_PER_SEC);
+        printf("%li,%f\n",n,(double)tim2[it]);
 
-   printf("How many numbers are you going to enter?: ");
-   scanf("%d",&count);
-
-   printf("Enter %d elements: ", count);
-   for(i=0;i<count;i++)
-      scanf("%d",&number[i]);
-
-   for(i=1;i<count;i++){
-      temp=number[i];
-      j=i-1;
-      while((temp<number[j])&&(j>=0)){
-         number[j+1]=number[j];
-         j=j-1;
-      }
-      number[j+1]=temp;
-   }
-
-   printf("Order of Sorted elements: ");
-   for(i=0;i<count;i++)
-      printf(" %d",number[i]);
-
-   return 0;
+        n += 10000;
+    }
+  
+    return 0;
 }
